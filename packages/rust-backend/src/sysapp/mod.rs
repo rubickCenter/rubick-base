db8eb19cbd6b262e84c585129f17f938fe684df8
sovlookup <gonorth@qq.com>
2021-10-14 10:40:13 +0800

    :construction_worker: update rust ci

--- packages/rust-backend/src/sysapp/mod.rs
+++ packages/rust-backend/src/sysapp/mod.rs
@@ -14,14 +14,14 @@ pub struct SearchResult {
 
 // only linux can returns detail info now, parsing windows/macos shortcut app info is still need to be done
 #[allow(dead_code)]
-pub fn find_apps(detail_json: bool, extra_dirs: Option<Vec<String>>) -> Vec<String> {
+pub fn find_apps(_detail_json: bool, extra_dirs: Option<Vec<String>>) -> Vec<String> {
     let extra_dirs = match extra_dirs {
         Some(dir) => dir,
         None => vec![],
     };
 
     #[cfg(target_os = "linux")]
-    let apps = linux::find_apps_linux(detail_json, extra_dirs);
+    let apps = linux::find_apps_linux(_detail_json, extra_dirs);
 
     #[cfg(target_os = "windows")]
     let apps = windows::find_apps_windows(extra_dirs);
